e66c0a16907deacca6c8cd8dedd65ebf39a6e9e9
SunnyLinSD <474289538@qq.com>
2016-03-27 10:33:29 +0800

    初始化项目

---
+++ zhihuDaily/zhihuDaily/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhihuDaily
+//
+//  Created by Sunny on 16/3/27.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhihuDaily/zhihuDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhihuDaily
+//
+//  Created by Sunny on 16/3/27.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
